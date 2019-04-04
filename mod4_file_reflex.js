81f76495b611206773d44b6d8aba01c9a9b459cb
Alfredo Campelo <egofrido@gmail.com>
2019-04-04 21:31:31 +0200

    add código función reflexes()

--- mod4_file_reflex.js
+++ mod4_file_reflex.js
@@ -1,18 +1,28 @@
 
- // Imports user module mod4-quote_of_the_day.js
+ // Imports user module mod4_quote_of_the_day.js
 let my_mod = require("./mod4_quote_of_the_day");
 
 let fs = require("fs");   // Imports file system module
 
 console.log();
 my_mod.quote_of_the_day();
-
+console.log();
+// Variable que calcula el retardo que se utilizara en setTimeout()
 let delay = ((Math.ceil(Math.random()*5))*1000).toFixed(0);
+//Transcurrido 'delay' desde la línea en blanco llama a reflexes()
 setTimeout(reflexes, delay);
 
 
 function reflexes() {
-
-    // ... include code here
+	console.log('Press Return');
+	// Variable con el tiempo de comienzo
+    let start = new Date().getTime();
+    process.stdin.on('data', () => {
+    	// Variable con el tiempo transcurrido desde el comienzo hasta 
+    	// la pulsación de return
+    	let stop = new Date().getTime() - start;
+    	console.log(`Tiempo transcurrido ${stop} milisegundos`);
+    	process.exit();
+    })
 };
 
--- mod4_quote_of_the_day.js
+++ mod4_quote_of_the_day.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 
-exports.quote_of_the_day = function (){
+exports.quote_of_the_day =  () => {
+	// archivo a leer
 	let _archivo = 'mod4-quotes_of_the_day.json';
 	fs.readFile(
 		_archivo,
@@ -8,6 +9,7 @@ exports.quote_of_the_day = function (){
 			if(err) throw err;
 			let _citas = JSON.parse(data);			
 			if(_citas.length > 0){
+				// Halla un número aletorio que sera el indice de la cita a mostrar
 				let i = (Math.floor(Math.random() * _citas.length)).toFixed(0);				
 				console.log("Quote of the day: ");
 				console.log(_citas[i]);
